f45f30dab60da0edf70a61cc3e0afdd2062aa7f5
Pedro Freitas de Lima <pedro.freitas.lima.97@gmail.com>
2024-09-12 17:12:24 -0300

    FIX: files not include before

---
+++ Bitstamp.LiveOrderBook.WorkerService/Program.cs
@@ -0,0 +1,7 @@
+using Bitstamp.LiveOrderBook.WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
