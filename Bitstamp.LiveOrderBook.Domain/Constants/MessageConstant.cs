68fe2dde239d8b69e7f46f75c4933d36106e071e
Pedro Freitas de Lima <pedro.freitas.lima.97@gmail.com>
2024-09-12 15:55:07 -0300

    CHORE: updated constants

--- Bitstamp.LiveOrderBook.Domain/Constants/MessageConstant.cs
+++ Bitstamp.LiveOrderBook.Domain/Constants/MessageConstant.cs
@@ -4,21 +4,15 @@ public static partial class MessageConstant
 {
     public static class MessageError
     {
-        
+        public static string InConnectionWebSocket = $"Error to connect in web socket in connection string: {SharedConstant.WebSocketConfiguration.ConnectionString}";
+        public static string InConnectionRabbitMQ = "Error to connect in rabbitmq in connection string:";
+        public static string InConnectionKafka = "Error to connect in kafka in connection string:";
+        public static string InConnectionMongoDB = "Error to connect in mongodb in connection string:";
+        public static string InConnectionDynamoDB = "Error to connect in dynamodb in connection string:";
     }
     
     public static class MessageInfo
     {
-        
-    }
-
-    public static class MessageWarning
-    {
-        
-    }
-
-    public static class MessageCritical
-    {
-        
+        public static string StartProccess = $"Worker started at: {DateTime.Now}";
     }
 }
